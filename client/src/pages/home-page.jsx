674475bf12b6facb04537992b84437060e83a2b9
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-02-13 20:46:26 +0200

    text

--- client/src/pages/home-page.jsx
+++ client/src/pages/home-page.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Grid, Box } from '@mui/material';
+import {
+  Grid, Box, Typography, Container,
+} from '@mui/material';
 import Banner from '../components/partials/home-page-banner/index';
 
 const HomePage = () => (
@@ -7,14 +9,29 @@ const HomePage = () => (
     <Box height="80vh">
       <Banner />
     </Box>
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <Box>xs=8</Box>
+    <Container>
+
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Typography>
+              Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industrys
+              standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived
+              not only five centuries, but also the leap into electronic
+              typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset
+              sheets containing Lorem Ipsum passages, and more recently
+              with desktop publishing software like Aldus PageMaker
+              including versions of Lorem Ipsum.
+            </Typography>
+          </Grid>
+          <Grid item xs={4} />
         </Grid>
-        <Grid item xs={4} />
-      </Grid>
-    </Box>
+      </Box>
+    </Container>
   </>
 );
 

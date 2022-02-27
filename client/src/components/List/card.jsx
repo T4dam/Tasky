40b28546ca20afd04aed744b8ed64be63af1e0bf
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-02-27 20:36:28 +0200

    icons at card

--- client/src/components/List/card.jsx
+++ client/src/components/List/card.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import {
   styled,
   Card as TextCard,
-  CardContent,
   Typography,
   IconButton,
   Box,
 } from '@mui/material';
 import { Draggable } from 'react-beautiful-dnd';
 import CancelIcon from '@mui/icons-material/Cancel';
+import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
 
 const Cardy = styled(TextCard)(({ theme }) => ({
   marginBottom: theme.spacing(1),
+  paddingBottom: 0,
 }));
 
-const CardyContainer = styled(CardContent)(({ theme }) => ({
+const CardyContainer = styled(Box)(({ theme }) => ({
   padding: theme.spacing(1),
   paddingRight: 0,
-
+  paddingBottom: '3px',
+  paddingTop: '3px',
 }));
 const Card = ({ card, index }) => (
   <Draggable draggableId={card.id} index={index}>
@@ -29,9 +31,12 @@ const Card = ({ card, index }) => (
       >
         <CardyContainer sx={{ display: 'flex', justifyContent: 'space-between' }}>
           <Typography>{card.content}</Typography>
-          <Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', paddingBottom: 0 }}>
             <IconButton size="small">
               <CancelIcon fontSize="small" />
+            </IconButton>
+            <IconButton size="small">
+              <DriveFileRenameOutlineIcon fontSize="small" />
             </IconButton>
           </Box>
         </CardyContainer>

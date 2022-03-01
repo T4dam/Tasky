491da1b245c9e06bc1943bfe1eb26d8e18014ce2
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-03-01 20:03:54 +0200

    error page

--- client/src/components/Layouts/drawer-mobile.jsx
+++ client/src/components/Layouts/drawer-mobile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import {
   Drawer as MUIDrawer,
@@ -7,45 +8,89 @@ import {
   List,
   ListItemIcon,
   ListItemText,
+  IconButton,
+  Divider,
+  Tooltip,
+  useTheme,
+  Box,
 } from '@mui/material';
-import AcUnitIcon from '@mui/icons-material/AcUnit';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
 import routes from '../../Routes/routes';
+import DrawerHeader from './dashboard-layout/dashboard-drawer-header';
+import { selectAuth } from '../../store/auth';
 
 const DrawerMobile = ({ open, handleDrawerClose }) => {
+  const state = useSelector(selectAuth);
+  const theme = useTheme();
   const navigate = useNavigate();
 
-  const handleRouteRegister = () => {
-    navigate(routes.LoginPage);
-  };
-  const itemsList = [
-    {
-      text: 'Pagrindinis',
-      icon: <AcUnitIcon />,
-      onClick: () => handleRouteRegister(),
-    },
-    {
-      text: <Link to={routes.RegisterPage}>Registruotis</Link>,
-      icon: <AcUnitIcon />,
-      onClick: () => handleRouteRegister(),
-    },
-    {
-      text: <Link to={routes.LoginPage}>Prisijungt</Link>,
-      icon: <AcUnitIcon />,
-      onClick: () => handleRouteRegister(),
-    },
-  ];
+  //   const handleRouteRegister = () => {
+  //     navigate(routes.LoginPage);
+  //   };
   return (
     <MUIDrawer variant="temporary" open={open} onClose={handleDrawerClose}>
+      <DrawerHeader>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+        </IconButton>
+      </DrawerHeader>
+      <Divider />
+
       <List>
-        {itemsList.map((item) => {
-          const { text, icon, onClick } = item;
-          return (
-            <ListItem button key={text} onClick={onClick}>
-              {icon && <ListItemIcon>{icon}</ListItemIcon>}
-              <ListItemText primary={text} />
-            </ListItem>
-          );
-        })}
+        <ListItem button onClick={() => navigate(routes.HomePage)}>
+          <ListItemIcon>
+            <Tooltip title="Pagrindinis puslapis" placement="right">
+              <HomeIcon />
+            </Tooltip>
+          </ListItemIcon>
+          <ListItemText primary="Pagrindinis puslapis" />
+        </ListItem>
+        {
+             state.loggedIn
+               ? (
+                 <Box>
+                   <ListItem button onClick={() => navigate(routes.BoardPage)}>
+                     <ListItemIcon>
+                       <Tooltip title="Planuoklė" placement="right">
+                         <InfoIcon />
+                       </Tooltip>
+                     </ListItemIcon>
+                     <ListItemText primary="Planuoklė" />
+                   </ListItem>
+                   <ListItem button onClick={() => navigate(routes.AboutPage)}>
+                     <ListItemIcon>
+                       <Tooltip title="Apie projektą" placement="right">
+                         <InfoIcon />
+                       </Tooltip>
+                     </ListItemIcon>
+                     <ListItemText primary="Apie projektą" />
+                   </ListItem>
+                 </Box>
+               )
+               : (
+                 <Box>
+                   <ListItem button onClick={() => navigate(routes.RegisterPage)}>
+                     <ListItemIcon>
+                       <Tooltip title="Registracija" placement="right">
+                         <InfoIcon />
+                       </Tooltip>
+                     </ListItemIcon>
+                     <ListItemText primary="Registracija" />
+                   </ListItem>
+                   <ListItem button onClick={() => navigate(routes.LoginPage)}>
+                     <ListItemIcon>
+                       <Tooltip title="Prisijungimas" placement="right">
+                         <InfoIcon />
+                       </Tooltip>
+                     </ListItemIcon>
+                     <ListItemText primary="Prisijungimas" />
+                   </ListItem>
+                 </Box>
+               )
+}
       </List>
     </MUIDrawer>
   );
--- client/src/pages/error-page.jsx
+++ client/src/pages/error-page.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Box, Container, styled } from '@mui/material';
+
+// eslint-disable-next-line no-unused-vars
+const Error = styled('img')(({ theme }) => ({
+  maxHeight: '500px',
+  width: '100%',
+  objectFit: 'cover',
+  objectPosition: 'cover',
+
+}));
 
 const ErrorPage = () => (
-  <h1>Error 404</h1>
+  <Container>
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Box sx={{ my: '15px' }}>
+        <Error
+          src="/404.png"
+          alt="profile background"
+        />
+      </Box>
+    </Box>
+  </Container>
 );
 
 export default ErrorPage;

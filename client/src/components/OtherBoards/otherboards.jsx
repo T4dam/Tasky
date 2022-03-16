cfc38763440526759bef7b6c4daf67a3a6244eb8
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-03-16 11:51:10 +0200

    otherBoards slight fix

--- client/src/components/OtherBoards/otherboards.jsx
+++ client/src/components/OtherBoards/otherboards.jsx
@@ -34,84 +34,72 @@ objectFit: 'cover',
 });
 
 const OtherBoards = ({ handleCloseOB }) => (
-    <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
-         <Button onClick={handleCloseOB}>
-<Box>
-{boardArr.map((item) => {
+//     <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
+//          <Button onClick={handleCloseOB}>
+// <Box>
+// {boardArr.map((item) => {
                
-                return <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+//                 return <Box key={item} sx={{ display: 'flex' }}>
                      
-                        <Picture src= {item.img} />
-                        <Typography sx={{
-                          maxWidth: '100px', color: 'black', lineHeight: 1.1, mt: '7px',
-                        }}
-                        >
-                          {item.text}
-                        </Typography>
-                      </Box>
-              })}
-</Box>
-</Button>
-</Box>
-//   <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
-//     <Button onClick={handleCloseOB}>
+//                         <Picture src= {item.img} />
+//                         <Typography sx={{
+//                           maxWidth: '100px', color: 'black', lineHeight: 1.1, mt: '7px',
+//                         }}
+//                         >
+//                           {item.text}
+//                         </Typography>
+//                       </Box>
+//               })}
+// </Box>
+// </Button>
+// </Box>
+  <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
+    <Button onClick={handleCloseOB}>
 
-//       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-//         <Box sx={{
-//           height: '100px', width: '100px', background: 'red', borderRadius: '5px',
-//         }}
-//         />
-//         <Typography sx={{
-//           maxWidth: '100px', color: 'black', lineHeight: 1.1, mt: '7px',
-//         }}
-//         >
-//           Pradėti naują
-//         </Typography>
-//       </Box>
-//     </Button>
-//     <Button onClick={handleCloseOB}>
-//       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-//         <Box sx={{
-//           height: '100px', width: '100px', background: 'red', borderRadius: '5px',
-//         }}
-//         />
-//         <Typography sx={{
-//           maxWidth: '100px', color: 'black', lineHeight: 1.1, mt: '7px',
-//         }}
-//         >
-//           Produkto išleidimas
-//         </Typography>
-//       </Box>
-//     </Button>
-//     <Button onClick={handleCloseOB}>
-//       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-//         <Box sx={{
-//           height: '100px', width: '100px', background: 'red', borderRadius: '5px',
-//         }}
-//         />
-//         <Typography sx={{
-//           maxWidth: '100px', color: 'black', lineHeight: 1.1, mt: '7px',
-//         }}
-//         >
-//           Projekto valdymas
-//         </Typography>
-//       </Box>
-//     </Button>
-//     <Button onClick={handleCloseOB}>
-//       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-//         <Box sx={{
-//           height: '100px', width: '100px', background: 'red', borderRadius: '5px',
-//         }}
-//         />
-//         <Typography sx={{
-//           maxWidth: '100px', color: 'black', lineHeight: 1.1, mt: '7px',
-//         }}
-//         >
-//           Kasdieniai darbai
-//         </Typography>
-//       </Box>
-//     </Button>
-//   </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <Picture src='https://unsplash.it/100/100'/>
+        <Typography sx={{
+          maxWidth: '100px', color: 'black', lineHeight: 1.1, mt: '7px',
+        }}
+        >
+          Pradėti naują
+        </Typography>
+      </Box>
+    </Button>
+    <Button onClick={handleCloseOB}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <Picture src='https://unsplash.it/100/101'/>
+        <Typography sx={{
+          maxWidth: '100px', color: 'black', lineHeight: 1.1, mt: '7px',
+        }}
+        >
+          Produkto išleidimas
+        </Typography>
+      </Box>
+    </Button>
+    <Button onClick={handleCloseOB}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <Picture src='https://unsplash.it/100/102'/>
+        <Typography sx={{
+          maxWidth: '100px', color: 'black', lineHeight: 1.1, mt: '7px',
+        }}
+        >
+          Projekto valdymas
+        </Typography>
+      </Box>
+    </Button>
+    <Button onClick={handleCloseOB}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <Picture src='https://unsplash.it/100/99'/>
+        <Typography sx={{
+          maxWidth: '100px', color: 'black', lineHeight: 1.1, mt: '7px',
+        }}
+        >
+          Kasdieniai darbai
+        </Typography>
+      </Box>
+    </Button>
+  </Box>
 );
 
 export default OtherBoards;
